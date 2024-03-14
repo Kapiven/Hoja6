5c9dace1139fe8d1511767d0b7cf563be64970ef
Kapiven <pin231132@uvg.edu.gt>
2024-03-14 11:38:03 -0600

    Se carga la factory para las funciones hash

---
+++ FactoryHash.java
@@ -0,0 +1,19 @@
+package Hoja6;
+public class FactoryHash {
+    
+        public IHash crearFuncion(String input) {
+            
+            if (input.equalsIgnoreCase("organica")) {
+                return new Organica();
+
+            } else if (input.equalsIgnoreCase("md5")) {
+                return new MD5();
+
+            } else if (input.equalsIgnoreCase("sha1")) {
+                return new SHA1();
+
+            } else {
+                throw new IllegalArgumentException("Tipo de función hash no válido");
+            }
+        }
+}
