a8033a507f23bcbe4f02a6ad664f4293ab94b15d
Yee404 <yee231193@uvg.edu.gt>
2024-03-14 11:07:24 -0600

    Commit1
    Crear el Factory para los Mapas: HashMap, TreeMap, LinkedHashMap

---
+++ FactoryMap.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class FactoryMap<K, V> {
+    public Map<K, V> getMap(int opcion) {
+        switch (opcion) {
+            case 1:
+                return new HashMap<K, V>();
+            case 2:
+                return new TreeMap<K, V>();
+            case 3:
+                return new LinkedHashMap<K, V>();
+            default:
+                return null;
+        }
+    }
+}
