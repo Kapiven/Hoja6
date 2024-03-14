ee79d927411c685180a6f0bc49f6d529c48ea319
Kapiven <pin231132@uvg.edu.gt>
2024-03-14 11:45:20 -0600

    Clase para MD5

---
+++ MD5.java
@@ -0,0 +1,11 @@
+package Hoja6;
+
+public class MD5 implements IHash{
+
+    @Override
+    public String calcularHash(String input) {
+        // Implementa la lógica para la función MD5
+        return "Código hash MD5";
+    }
+    
+}
