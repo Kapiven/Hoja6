51c240536abbd6e0eab0be05d250939e44926883
Kapiven <pin231132@uvg.edu.gt>
2024-03-14 11:46:31 -0600

    Clase para hash organica

---
+++ Organica.java
@@ -0,0 +1,11 @@
+package Hoja6;
+
+public class Organica implements IHash {
+
+    @Override
+    public String calcularHash(String input) {
+        // Implementa la lógica para la función orgánica
+        return "Código hash organica";
+    }
+    
+}
